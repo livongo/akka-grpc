31c7814acf115a2168252b09af0c0d822a8c56d0
ctoomey <ctoomey@gmail.com>
2019-01-17 15:29:05 -0800

    #448 custom exception handler

--- runtime/src/main/scala/akka/grpc/scaladsl/GrpcExceptionHandler.scala
+++ runtime/src/main/scala/akka/grpc/scaladsl/GrpcExceptionHandler.scala
@@ -11,7 +11,16 @@ import akka.grpc.internal.GrpcResponseHelpers
 import akka.http.scaladsl.model.HttpResponse
 
 object GrpcExceptionHandler {
+  def default(mapper: PartialFunction[Throwable, Status]): PartialFunction[Throwable, Future[HttpResponse]] = {
+    mapper
+      .orElse(defaultMapper)
+      .andThen(s => Future.successful(GrpcResponseHelpers.status(s)))
+  }
+
   val defaultMapper: PartialFunction[Throwable, Status] = {
+    case e: ExecutionException ⇒
+      if (e.getCause == null) Status.INTERNAL
+      else defaultMapper(e.getCause)
     case grpcException: GrpcServiceException ⇒ grpcException.status
     case _: NotImplementedError ⇒ Status.UNIMPLEMENTED
     case _: UnsupportedOperationException ⇒ Status.UNIMPLEMENTED
@@ -19,23 +28,4 @@ object GrpcExceptionHandler {
   }
 
   val default: PartialFunction[Throwable, Future[HttpResponse]] = default(defaultMapper)
-
-  def default(mapper: PartialFunction[Throwable, Status]): PartialFunction[Throwable, Future[HttpResponse]] = {
-    case e: ExecutionException ⇒
-      if (e.getCause == null) Future.failed(e)
-      else handling(mapper)(e.getCause)
-    case other ⇒
-      handling(mapper)(other)
-  }
-
-  private def handling(mapper: PartialFunction[Throwable, Status]): PartialFunction[Throwable, Future[HttpResponse]] = {
-    mapper
-      .orElse[Throwable, Status] {
-        case grpcException: GrpcServiceException ⇒ grpcException.status
-        case _: NotImplementedError ⇒ Status.UNIMPLEMENTED
-        case _: UnsupportedOperationException ⇒ Status.UNIMPLEMENTED
-        case other ⇒ Status.INTERNAL
-      }
-      .andThen(s => Future.successful(GrpcResponseHelpers.status(s)))
-  }
 }
